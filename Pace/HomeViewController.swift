256f56436ea05b939f9ccfed95bdb1a3ee9d426a
thenameisangel <y.lo@utexas.edu>
2016-11-10 08:48:10 -0600

    Initiated SDK Integration

---
+++ Pace/HomeViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Pace
+//
+//  Created by Angel Lo on 11/8/16.
+//  Copyright © 2016 Angel. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var loginButton: UIButton!
+    @IBAction func loginWithSpotify(sender: AnyObject) {
+        //create URL to open Safari window
+        
+        //create authentication obj
+        //let auth = SPTAuth.defaultInstance()
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
