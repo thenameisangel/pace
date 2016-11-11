7c78cfa58649ad6cd8374096bc71982a959c8b00
Nednur <an23355@lib-pclem048.lib.utexas.edu>
2016-11-10 21:13:58 -0600

    Added Basic Versions of Adarsh's Views

---
+++ Pace/SearchSpotifyButton.swift
@@ -0,0 +1,21 @@
+//
+//  SearchSpotifyButton.swift
+//  Pace
+//
+//  Created by Nednur, Adarsh on 11/10/16.
+//  Copyright © 2016 Angel. All rights reserved.
+//
+
+import UIKit
+
+class SearchSpotifyButton: UIButton {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
