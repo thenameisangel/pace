e08693495a8fe5dde260d38093a5ffad7142b71d
Kavina <kavinapandya211@gmail.com>
2016-11-13 13:38:19 -0600

    Added past runs tableview

---
+++ Pace/PastRunTableViewCell.swift
@@ -0,0 +1,31 @@
+//
+//  PastRunTableViewCell.swift
+//  Pace
+//
+//  Created by Kavina Pandya on 11/13/16.
+//  Copyright © 2016 Angel. All rights reserved.
+//
+
+import UIKit
+
+class PastRunTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var runDateLbl: UILabel!
+    @IBOutlet weak var distanceLbl: UILabel!
+    @IBOutlet weak var paceLbl: UILabel!
+    @IBOutlet weak var timeLbl: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
