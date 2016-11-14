b46c3640763f14e0d227b3caeb07afec1a222301
Neil Desai <ndesai1996@gmail.com>
2016-11-14 16:42:42 -0600

    added PlaylistSongCell

---
+++ Pace/PlaylistSongCell.swift
@@ -0,0 +1,13 @@
+//
+//  PlaylistSongCell.swift
+//  Pace
+//
+//  Created by Neil Desai on 11/14/16.
+//  Copyright © 2016 Angel. All rights reserved.
+//
+
+import Cocoa
+
+class PlaylistSongCell: UITableViewCell {
+
+}
