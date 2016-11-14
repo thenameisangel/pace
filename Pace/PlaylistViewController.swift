91cc8dcc792bb3dd4d1c5aa6e533932f1edf8767
Neil Desai <ndesai1996@gmail.com>
2016-11-13 21:05:35 -0600

    added PlaylistViewController file

---
+++ Pace/PlaylistViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PlaylistViewController.swift
+//  Pace
+//
+//  Created by Neil Desai on 11/13/16.
+//  Copyright © 2016 Angel. All rights reserved.
+//
+
+import Cocoa
+
+class PlaylistViewController: UIViewController {
+
+}
