b4cdbba525dae6b734b47343f15f3d1d71ddcfee
thenameisangel <y.lo@utexas.edu>
2016-11-19 16:35:23 -0600

    imported entire SDK in Obj-C bridging header

--- Pace/HomeViewController.swift
+++ Pace/HomeViewController.swift
@@ -5,28 +5,83 @@
 //  Created by Angel Lo on 11/8/16.
 //  Copyright © 2016 Angel. All rights reserved.
 //
-
 import UIKit
 
 class HomeViewController: UIViewController {
-
+    let clientID = "7884d212f6ef45b5b4691688644f1217"
+    let callURL = "pace://returnAfterLogin"
+    let tokenSwapURL = "http://localhost:1234/swap"
+    let tokenRefreshServiceURL = "http://localhost:1234/refresh"
+    
+    var session:SPTSession!
+    var player:SPTAudioStreamingController?
+    
     @IBOutlet weak var loginButton: UIButton!
-    @IBAction func loginWithSpotify(sender: AnyObject) {
-        //create URL to open Safari window
-        
-        //create authentication obj
-        let auth = SPTAuth.defaultInstance()
-    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loginButton.hidden = true
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(HomeViewController.updateAfterFirstLogin), name: "loginSuccessful", object: nil)
+        
+        let userDefaults = NSUserDefaults.standardUserDefaults()
+        
+        //session already exists
+        if let sessionObj:AnyObject = userDefaults.objectForKey("SpotifySession") {
+            let sessionDataObj = sessionObj as! NSData
+            
+            let session = NSKeyedUnarchiver.unarchiveObjectWithData(sessionDataObj) as! SPTSession
+            
+            if !session.isValid() {
+                SPTAuth.defaultInstance().renewSession(session, callback: { (error:NSError!, session:SPTSession!) -> Void in
+                    if error == nil {
+                        let sessionData = NSKeyedArchiver.archivedDataWithRootObject(session)
+                        userDefaults.setObject(sessionData, forKey: "SpotifySession")
+                        userDefaults.synchronize()
+                        
+                        self.session = session
+                        //playUsingSession()
+                    } else {
+                        print("Error refreshing session")
+                    }
+                })
+            } else {
+                print("session valid")
+                //playingUsingSession()
+            }
+        } else {
+            //session doesn't exist
+            loginButton.hidden = false
+        }
+        
     }
-
+    
+    func updateAfterFirstLogin() {
+        
+        loginButton.hidden = true
+        
+        
+    }
+    
+    
+    func playUsingSession(sessionObj:SPTSession!) {
+    
+        
+        
+    }
+    
+    @IBAction func loginWithSpotify(sender: AnyObject) {
+        
+        //create URL to open Safari window
+        let loginURL = SPTAuth.loginURLForClientId(clientID, withRedirectURL: NSURL(string: callURL), scopes: [SPTAuthStreamingScope], responseType: "token")
+        
+        UIApplication.sharedApplication().openURL(loginURL)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
--- Pace/Pace-Bridging-Header.h
+++ Pace/Pace-Bridging-Header.h
@@ -1,5 +1,53 @@
+
 //
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
+//import Spotify Authentication Framework
+#import <SpotifyAuthentication/SpotifyAuthentication.h>
 #import <SpotifyAuthentication/SPTAuth.h>
+#import <SpotifyAuthentication/SPTAuthViewController.h>
+#import <SpotifyAuthentication/SPTConnectButton.h>
+#import <SpotifyAuthentication/SPTEmbeddedImages.h>
+#import <SpotifyAuthentication/SPTSession.h>
+#import <SpotifyAuthentication/SPTStoreViewController.h>
+
+//import SpotifyAudioPlayback Framework
+#import <SpotifyAudioPlayback/SpotifyAudioPlayback.h>
+#import <SpotifyAudioPlayback/SpPlaybackEvent.h>
+#import <SpotifyAudioPlayback/SPTAudioPlaybackTypes.h>
+#import <SpotifyAudioPlayback/SPTAudioStreamingController_ErrorCodes.h>
+#import <SpotifyAudioPlayback/SPTAudioStreamingController.h>
+#import <SpotifyAudioPlayback/SPTCircularBuffer.h>
+#import <SpotifyAudioPlayback/SPTCoreAudioController.h>
+#import <SpotifyAudioPlayback/SPTDiskCache.h>
+#import <SpotifyAudioPlayback/SPTDiskCaching.h>
+#import <SpotifyAudioPlayback/SPTPlaybackMetadata.h>
+#import <SpotifyAudioPlayback/SPTPlaybackState.h>
+
+//import SpotifyMetadata framework
+#import <SpotifyMetadata/SpotifyMetadata.h>
+#import <SpotifyMetadata/SPTAlbum.h>
+#import <SpotifyMetadata/SPTArtist.h>
+#import <SpotifyMetadata/SPTBrowse.h>
+#import <SpotifyMetadata/SPTFeaturedPlaylistList.h>
+#import <SpotifyMetadata/SPTFollow.h>
+#import <SpotifyMetadata/SPTImage.h>
+#import <SpotifyMetadata/SPTJSONDecoding.h>
+#import <SpotifyMetadata/SPTListPage.h>
+#import <SpotifyMetadata/SPTMetadataTypes.h>
+#import <SpotifyMetadata/SPTPartialAlbum.h>
+#import <SpotifyMetadata/SPTPartialArtist.h>
+#import <SpotifyMetadata/SPTPartialObject.h>
+#import <SpotifyMetadata/SPTPartialPlaylist.h>
+#import <SpotifyMetadata/SPTPartialTrack.h>
+#import <SpotifyMetadata/SPTPlaylistList.h>
+#import <SpotifyMetadata/SPTPlaylistSnapshot.h>
+#import <SpotifyMetadata/SPTPlaylistTrack.h>
+#import <SpotifyMetadata/SPTRequest.h>
+#import <SpotifyMetadata/SPTSavedTrack.h>
+#import <SpotifyMetadata/SPTSearch.h>
+#import <SpotifyMetadata/SPTTrack.h>
+#import <SpotifyMetadata/SPTTrackProvider.h>
+#import <SpotifyMetadata/SPTUser.h>
+#import <SpotifyMetadata/SPTYourMusic.h>
