2f042e31bf5671ef8e43ff365cee1338934a865b
Neil Desai <ndesai1996@gmail.com>
2016-12-04 19:24:05 -0600

    RunTracker functionality

--- Pace/RunSummaryViewController.swift
+++ Pace/RunSummaryViewController.swift
@@ -10,32 +10,125 @@ import Foundation
 import UIKit
 import CoreLocation
 import HealthKit
+import CoreData
+import MapKit
 
 class RunSummaryViewController: UIViewController {
     
-    @IBOutlet weak var totalRunTimeLbl: UITextView!
-    @IBOutlet weak var totalRunDistanceLbl: UITextView!
-    @IBOutlet weak var targetPaceLbl: UITextView!
-    @IBOutlet weak var viewPlaylistBtn: UIButton!
-    @IBAction func viewPlaylist(sender: AnyObject) {
-    }
+    // MARK: Properties
+    @IBOutlet weak var mapView: MKMapView!
+
+    @IBOutlet weak var distanceLabel: UITextView!
+    @IBOutlet weak var durationLabel: UITextView!
+    @IBOutlet weak var paceLabel: UITextView!
+    @IBOutlet weak var targetpaceLabel: UITextView!
     
-    var seconds: Double!
-    var distance: Double!
-    var targetPace: Double!
+    // MARK: Actions
+    @IBAction func viewRuns(sender: AnyObject) {
+    }
+
+    @IBOutlet weak var viewPlaylist: UIButton!
+    
+    // MARK: Prepare Segues
+    
+    // MARK: Variables
+    var run: NSManagedObject!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setLabels()
+        loadMap()
+    }
+    
+    // MARK: Set Labels
+    func setLabels() {
+        let distance = run.valueForKey("distance") as! Float
+        let duration = run.valueForKey("duration") as! Float
+        
+        distanceLabel.text = String(distance)
+        durationLabel.text = String(duration)
+        paceLabel.text = String(distance/duration)
+        
+        // Label Formatting
+        distanceLabel.font = UIFont(name: "Avenir", size: 17)
+        distanceLabel.textAlignment = .Center
+        durationLabel.font = UIFont(name: "Avenir", size: 17)
+        durationLabel.textAlignment = .Center
+        paceLabel.font = UIFont(name: "Avenir", size: 17)
+        paceLabel.textAlignment = .Center
+        targetpaceLabel.font = UIFont(name: "Avenir", size: 17)
+        targetpaceLabel.textAlignment = .Center
+    }
+    
+    // MARK: Generate Map Overlay
+    
+    // Map Region
+    func mapRegion() -> MKCoordinateRegion{
+        
+        let locations = run.valueForKey("locations") as! NSSet
+        let initialLocation = locations.allObjects.first
+        
+        var minLat = initialLocation!.valueForKey("latitude") as! Double
+        var minLong = initialLocation!.valueForKey("longitude") as! Double
+        var maxLat = initialLocation!.valueForKey("latitude") as! Double
+        var maxLong = initialLocation!.valueForKey("longitude") as! Double
+        
+        for location in locations {
+            minLat = min(minLat, location.valueForKey("latitude") as! Double)
+            minLong = min(minLong, location.valueForKey("longitude") as! Double)
+            maxLat = max(maxLat, location.valueForKey("latitude") as! Double)
+            maxLong = max(maxLong, location.valueForKey("longitude") as! Double)
+        }
+        
+        return MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: (minLat + maxLat)/2, longitude: (minLong + maxLong)/2),
+            span: MKCoordinateSpan(latitudeDelta: (maxLat - minLat)*1.5, longitudeDelta: (maxLong - minLong)*1.5))
+    }
+    
+    // Overlay Properties
+    func mapView(mapView: MKMapView!, rendererForOverlay overlay: MKOverlay!) -> MKOverlayRenderer! {
+        if overlay is MKPolyline {
+            let polylineRenderer = MKPolylineRenderer(overlay: overlay)
+            polylineRenderer.strokeColor = UIColor.blueColor()
+            polylineRenderer.lineWidth = 3
+            return polylineRenderer
+        }
+        
+        return nil
+    }
+    
+    // Overlay Path
+    func polyline() -> MKPolyline {
+        var coords = [CLLocationCoordinate2D]()
+        
+        let locations = run.valueForKey("locations") as! NSSet
+        
+        let descriptor = NSSortDescriptor(key: "timestamp", ascending: true)
+        let sortedlocations: NSArray = locations.sortedArrayUsingDescriptors([descriptor])
+        
+        for location in sortedlocations {
+            let coord = CLLocationCoordinate2D(latitude: location.valueForKey("latitude") as! Double, longitude: location.valueForKey("longitude") as! Double)
+            
+            coords.append(coord)
+        }
+        
+        return MKPolyline(coordinates: &coords, count: sortedlocations.count)
+    }
+    
+    // Load Map
+    func loadMap() {
+        let locations = run.valueForKey("locations") as! NSSet
+        
+        if locations.count > 0 {
+            mapView.hidden = false
+            
+            // Set the map bounds
+            mapView.region = mapRegion()
+            
+            // Make the line(s!) on the map
+            mapView.addOverlay(polyline())
+        }
+        
     }
 
-    
-    func setLabels() {
-        print(seconds)
-        print(distance)
-        print(targetPace)
-        totalRunTimeLbl.text = String(seconds) + " seconds"
-        totalRunDistanceLbl.text = String(distance) + " meters"
-        targetPaceLbl.text = String(targetPace) + " minutes per mile"
-    }
 }
--- Pace/RunTrackerViewController.swift
+++ Pace/RunTrackerViewController.swift
@@ -25,7 +25,6 @@ class RunTrackerViewController: UIViewController, SPTAudioStreamingDelegate, SPT
     var player: SPTAudioStreamingController?
     let homeVC:HomeViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("HomeViewController") as! HomeViewController
     
-    
     // MARK: Properties
     @IBOutlet weak var songTitleLbl: UITextView!
     @IBOutlet weak var artistLbl: UITextView!
@@ -36,10 +35,10 @@ class RunTrackerViewController: UIViewController, SPTAudioStreamingDelegate, SPT
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loginToPlayer()
-        loadSongData()
+        //loginToPlayer()
+        //loadSongData()
         
-        // MARK: Set font
+        // Set font
         distanceRunLbl.font = UIFont.boldSystemFontOfSize(85)
         timeElapsedLbl.font = UIFont(name: "Avenir", size: 17)
         targetPaceLbl.font = UIFont(name: "Avenir", size: 17)
@@ -74,17 +73,18 @@ class RunTrackerViewController: UIViewController, SPTAudioStreamingDelegate, SPT
     }
     
     // MARK: Actions
-    // Saving the Run
+    
+    // End Run Button
     @IBAction func endRun(sender: AnyObject) {
-        let savedRun = NSEntityDescription.insertNewObjectForEntityForName("Run", inManagedObjectContext: managedObjectContext)
-        savedRun.setValue(NSDate(), forKey: "date")
-        savedRun.setValue(Double(distance), forKey: "distance")
-        savedRun.setValue(Double(targetPace), forKey: "pace")
-        savedRun.setValue(Double(seconds), forKey: "time")
-        do {
-            try managedObjectContext.save()
-        } catch {
-            fatalError("Failure to save context: \(error)")
+        SaveRun()
+        self.performSegueWithIdentifier("endRunSegue", sender: self)
+    }
+    
+    // MARK: Prepare Segues
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        if segue.identifier == "endRunSegue"{
+            let vc = segue.destinationViewController as! RunSummaryViewController
+            vc.run = currentRun
         }
     }
     
@@ -92,20 +92,20 @@ class RunTrackerViewController: UIViewController, SPTAudioStreamingDelegate, SPT
     var targetPace: String!
     var seconds = 0.0
     var distance = 0.0
+    var currentRun: NSManagedObject!
+    var managedContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
     
     lazy var locations = [CLLocation]()
     lazy var timer = NSTimer()
     
     // MARK: Location Manager Properties
-    lazy var locationManager: CLLocationManager = {
+    lazy var locationManager: CLLocationManager = { //lazy establishes variable when first needed
         var _locationManager = CLLocationManager()
-        _locationManager.delegate = self
-        _locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        _locationManager.delegate = self //_locationManager delegate
+        _locationManager.desiredAccuracy = kCLLocationAccuracyBest //_locationManager properties
         _locationManager.activityType = .Fitness
-        
-        // Movement threshold for new events
         _locationManager.distanceFilter = 10.0
-        return _locationManager
+        return _locationManager //locationManager = _locationManager
     }()
     
     func startLocationUpdates() {
@@ -115,15 +115,13 @@ class RunTrackerViewController: UIViewController, SPTAudioStreamingDelegate, SPT
 
     // MARK: Entering the View
     override func viewWillAppear(animated: Bool) {
-        locationManager.requestAlwaysAuthorization() // authorize core location
+        super.viewWillAppear(animated)
+        locationManager.requestAlwaysAuthorization()
         
-        // initialize fields and timer
         seconds = 0.0
         distance = 0.0
         locations.removeAll(keepCapacity: false)
-       timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(RunTrackerViewController.eachSecond(_:)), userInfo: nil, repeats: true)
-        
-
+        timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: #selector(RunTrackerViewController.eachSecond(_:)), userInfo: nil, repeats: true)
         startLocationUpdates()
     }
     
@@ -141,14 +139,42 @@ class RunTrackerViewController: UIViewController, SPTAudioStreamingDelegate, SPT
         let distanceQuantity = HKQuantity(unit: HKUnit.meterUnit(), doubleValue: distance)
         distanceRunLbl.text = distanceQuantity.description
         
-        // MARK: Set font
+        // Set font
         distanceRunLbl.font = UIFont.boldSystemFontOfSize(85)
         timeElapsedLbl.font = UIFont(name: "Avenir", size: 17)
         timeElapsedLbl.textAlignment = .Center
         distanceRunLbl.textAlignment = .Center
-//        targetPaceLbl.text = targetPace + " minutes per mile"
     }
     
+    // MARK: Save Method
+    func SaveRun() {
+        timer.invalidate()
+        
+        let savedRun = NSEntityDescription.insertNewObjectForEntityForName("Run", inManagedObjectContext: managedContext)
+        savedRun.setValue(distance, forKey: "distance")
+        savedRun.setValue(seconds, forKey: "duration")
+        savedRun.setValue(NSDate(), forKey: "timestamp")
+        
+        currentRun = savedRun
+        
+        for location in locations {
+            let savedLocation = NSEntityDescription.insertNewObjectForEntityForName("Location", inManagedObjectContext: managedContext)
+            
+            savedLocation.setValue(location.timestamp, forKey: "timestamp")
+            savedLocation.setValue(location.coordinate.latitude, forKey: "latitude")
+            savedLocation.setValue(location.coordinate.longitude, forKey: "longitude")
+            
+            savedRun.mutableSetValueForKey("locations").addObject(savedLocation)
+        }
+        
+        do {
+            try managedContext.save()
+        } catch {
+            fatalError("Failure to save context: \(error)")
+        }
+    }
+    
+    // MARK: Audio
     func startNextSong() {
         let uri = playlist[0]["uri"] as! String
         
@@ -186,34 +212,19 @@ class RunTrackerViewController: UIViewController, SPTAudioStreamingDelegate, SPT
         print(error)
     }
     
-    let managedObjectContext = (UIApplication.sharedApplication().delegate as! AppDelegate).managedObjectContext
-    
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        if segue.identifier == "endRunSegue"{
-            let nextScene = segue.destinationViewController as? RunSummaryViewController
-            nextScene!.seconds = seconds
-            nextScene!.distance = distance
-            nextScene!.targetPace = Double(targetPace)
-        }
-    }
 }
 
-// MARK: - CLLocationManagerDelegate (Recording the Run)
+// MARK: - CLLocationManagerDelegate
 extension RunTrackerViewController: CLLocationManagerDelegate {
-    
     func locationManager(manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        for location in locations as [CLLocation] {
-            if location.horizontalAccuracy < 20 {
-                //update distance
-                if self.locations.count > 0 {
-                    distance += location.distanceFromLocation(self.locations.last!)
+        //print("recieved location")
+        for location in locations as [CLLocation]{ //locations contain current locations outputted by the location manager (usually 1 location)
+            if location.horizontalAccuracy < 20 { //consider outputting location if accurage enough
+                if self.locations.count > 0 { //self.locations contain all locations in run
+                    distance += location.distanceFromLocation(self.locations.last!) //add to total distance ran
                 }
-                
-                //save location
-                self.locations.append(location)
+                self.locations.append(location) //add outputting location to self.locations
             }
         }
     }
-
-    
 }
