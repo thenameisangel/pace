4e69d6e500ea3e6d6e559250fd9d2296c4b0e883
Neil Desai <ndesai1996@gmail.com>
2016-12-05 23:32:35 -0600

    picker

--- Pace/NewRunViewController.swift
+++ Pace/NewRunViewController.swift
@@ -9,7 +9,7 @@
 import Foundation
 import UIKit
 
-class NewRunViewController: UIViewController {
+class NewRunViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
     
     @IBOutlet weak var searchTermTxt: UITextField!
     @IBOutlet weak var selectGenreTxt: UITextField!
@@ -26,10 +26,53 @@ class NewRunViewController: UIViewController {
         self.performSegueWithIdentifier("PastRuns", sender: nil)
     }
     
+    // MARK: Pickers
+   
+    @IBOutlet weak var genrePicker: UIPickerView!
+    
+    let genres = [ "acoustic", "afrobeat", "alt-rock", "alternative", "ambient", "anime", "black-metal", "bluegrass", "blues", "bossanova", "brazil", "breakbeat", "british", "cantopop", "chicago-house", "children", "chill", "classical", "club", "comedy", "country", "dance", "dancehall", "death-metal", "deep-house", "detroit-techno", "disco", "disney", "drum-and-bass", "dub", "dubstep", "edm", "electro", "electronic", "emo", "folk", "forro", "french", "funk", "garage", "german", "gospel", "goth", "grindcore", "groove", "grunge", "guitar", "happy", "hard-rock", "hardcore", "hardstyle", "heavy-metal", "hip-hop", "holidays", "honky-tonk", "house", "idm", "indian", "indie", "indie-pop", "industrial", "iranian", "j-dance", "j-idol", "j-pop", "j-rock", "jazz", "k-pop", "kids", "latin", "latino", "malay", "mandopop", "metal", "metal-misc", "metalcore", "minimal-techno", "movies", "mpb", "new-age", "new-release", "opera", "pagode", "party", "philippines-opm", "piano", "pop", "pop-film", "post-dubstep", "power-pop", "progressive-house", "psych-rock", "punk", "punk-rock", "r-n-b", "rainy-day", "reggae", "reggaeton", "road-trip", "rock", "rock-n-roll", "rockabilly", "romance", "sad", "salsa", "samba", "sertanejo", "show-tunes", "singer-songwriter", "ska", "sleep", "songwriter", "soul", "soundtracks", "spanish", "study", "summer", "swedish", "synth-pop", "tango", "techno", "trance", "trip-hop", "turkish", "work-out", "world-music" ]
+    
+    @IBOutlet weak var heightPicker: UIPickerView!
+    
+    let heights = ["4' 0\"", "4' 1\"", "4' 2\"", "4' 3\"", "4' 4\"", "4' 5\"", "4' 6\"", "4' 7\"", "4' 8\"", "4' 9\"", "4' 10\"", "4' 11\"", "5' 0\"", "5' 1\"", "5' 2\"", "5' 3\"", "5' 4\"", "5' 5\"", "5' 6\"", "5' 7\"", "5' 8\"", "5' 9\"", "5' 10\"", "5' 11\"", "6' 0\"", "6' 1\"", "6' 2\"", "6' 3\"", "6' 4\"", "6' 5\"", "6' 6\"", "6' 7\"", "6' 8\"", "6' 9\"", "6' 10\"", "6' 11\"", ]
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if (pickerView.tag == 1) {
+            return genres.count
+        }
+        
+        else {
+            return heights.count
+        }
+    }
+    
+    
+    func pickerView(pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusingView view: UIView?) -> UIView {
+        
+        let label = UILabel()
+        
+        label.font = UIFont(name: "Avenir", size: 12.0)
+        label.textAlignment = .Center
+        
+        if (pickerView.tag == 1) {
+            label.text = genres[row]
+        }
+        
+        else {
+            label.text = heights[row]
+        }
+        return label
+    }
+
     var targetPace:String!
     
     override func viewDidLoad() {
         viewPastRunsBtn.layer.borderWidth = 0.0
+    
     }
     
     override func viewDidLayoutSubviews() {
